f7fc2d0698f13685577bd7c07f5f0474dfcd441b
Ashish Pandian <ashishpandian@berkeley.edu>
2022-12-26 18:45:00 -0800

    header files

--- bloom.h
+++ bloom.h
@@ -25,8 +25,54 @@ struct bloom
 };
 /*
 * initializes bloom filter
+* optimal number of bits = (entries * ln(error)) / ln(2)^2
+* optimal number of hash function k = bpe * ln(2) What is bpe
+* inputs:
+*   bloom - pointer to an allocated struct bloom 
+*   entries - expected number of entries which will be added
+*   error - probability of collision
 * 
+* return value: 0 sucesss, 1 error
 */
-int bloom_init2(struct bloom * bloom, unsigned int entries, double errors);
+int bloom_init(struct bloom * bloom, unsigned int entries, double errors);
 
+/*
+Check if element ecists in bloom filter(chance of false positive)
+inputs: 
+    buffer - pointer to buffer containing element to check
+    len - size of 'buffer'
+return value: 0 if element not present, 1 if present, -1 if bloom not initialized(bloom_init)
+*/
+int bloom_check(struct bloom * bloom, const void * buffer, int len);
+
+/*
+Adds element to the filter(bloom_check and then if not present, adds it)
+inputs:
+    buffer - pointer to buffer containing element to check
+    len - size of 'buffer'
+return value: 0 if element is not present and added, 1 if element already added, -1 if bloom not initialized
+*/
+int bloom_add(struct bloom * bloom, const void * buffer, int len);
+
+/*
+Prints bloom filter
+*/
+void bloom_print(struct bloom * bloom);
+
+/*
+Deallocates internal storage
+Bloom struct no longer usable and must call bloom_init to reinitalize it
+*/
+void bloom_free(struct bloom * bloom);
+
+/**
+ * Erase internal storage, but keeps intialized state(no need to call bloom_init)
+ * return value: 0 if sucess, 1 if failure
+*/
+int bloom_reset(struct bloom * bloom);
+
+#ifdef __cplusplus
 }
+#endif
+
+#endif
